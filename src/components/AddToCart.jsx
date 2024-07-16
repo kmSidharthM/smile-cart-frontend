6e3159efa736fa29cd084b55fccb636a4c74a916
kmSidharthM <kmsidharthmurali@gmail.com>
2024-07-16 22:20:19 +0530

    Added feature to add products to cart

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@bigbinary/neetoui";
+
+const AddToCart = ({ isInCart, toggleIsInCart }) => {
+  const handleClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    toggleIsInCart();
+  };
+
+  return (
+    <Button
+      label={isInCart ? "Remove from cart" : "Add to cart"}
+      size="large"
+      onClick={handleClick}
+    />
+  );
+};
+export default AddToCart;
